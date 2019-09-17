04f46568d040f2ed5db47afe9e1dec1d7d7a75bf
Armin <armin-karadzic@live.com>
2019-09-17 16:59:06 +0200

    Oppgave 1a fikset Oppgave 1b gjort ferdig

--- Oblig1/src/Oblig1.java
+++ Oblig1/src/Oblig1.java
@@ -4,23 +4,42 @@ public class Oblig1 {
 
     //Oppgave 1a
     public static int maks(int[] a) {
+        if(a.length == 0) {
+            throw new NoSuchElementException("Tabellen er tom");
+        }
         int maks = a[0];
 
         for(int i = 0; i < a.length; ++i) {
             if (a[i] > maks) {
                 maks = a[i];
             }
-            else if(a.length == 0) {
-                throw new NoSuchElementException("Tabellen er tom");
-            }
+
         }
         return maks;
     }
 
+    // Oppgave 2a
+    public static int ombyttinger(int[] a) {
+
+        int antall_ombyttinger = 0;
+        if (a.length == 0)
+            throw new NoSuchElementException("Tabellen er tom");
+        for(int i = 1; i < a.length; ++i) {
+            if (a[i - 1] > a[i]){
+                int maks = a[i - 1];
+                a[i - 1] = a[i];
+                a[i] = maks;
+                antall_ombyttinger++;
+            }
+        }
+        return antall_ombyttinger;
+    }
+
     public static void main(String[] args) {
-        int a[] = {};
+        int a[] = {5, 3, 6, 8,-5};
 
         System.out.println(Oblig1.maks(a));
+        System.out.println(Oblig1.ombyttinger(a));
     }
 
 }
--- Oblig1/src/Oblig1Test.java
+++ Oblig1/src/Oblig1Test.java
@@ -19,5 +19,23 @@ class Oblig1Test {
                 antallFeil++;
             }
         }
+
+        if (!unntak) {
+            System.out.println("Opgave 1: b) Kast unntak for en tom tabell!");
+            antallFeil++;
+        }
+
+        int[] a = {3};
+        int[] b = {5, 2, 8, 4, 7, 6};
+        int[] c = {5, 4, 3, 2, 1};
+        int[] d = {1, 2, 3, 4, 5};
+        if (Oblig1.maks(a) != 3 || Oblig1.maks(b) != 8 ||
+                Oblig1.maks(c) != 5 || Oblig1.maks(d) != 5) {
+            System.out.println("Oppgave 1: c) Maks-metoden: Feil resultat!");
+            antallFeil++;
+        }
+
+        int[] e = {1, 4, 3, 7, 6, 5, 10, 2, 9, 8};
+        int[] f = {1, 3, 4, 6, 5, 7, 2, 9, 8, 10};
     }
 }
